3875ddd045628325c460e1e46ae7d17a66ba1a82
mwsohn <mwsohn@gmail.com>
2026-08-26 23:47:19 -0400

    Update helper.jl

--- src/helper.jl
+++ src/helper.jl
@@ -13,7 +13,7 @@ function Base.show(io::IO, m::TAB1OUT)
         stubhead_label=string(m.varname),
         column_labels=["Counts", " Percent", "Cum Pct"],
         formatters=[(v, i, j) -> j in (2, 3) ? Printf.format(fmt, v) : @sprintf("%.0d", v)],
-        maximum_number_of_rows=m.maxrows,
+        maximum_number_of_rows = m.maxrows,
         table_format=TextTableFormat(;
             @text__no_vertical_lines,
             horizontal_lines_at_column_labels=[1],
@@ -28,7 +28,6 @@ struct TAB1OUT2
     varname::String
     digits::Int8
     maxrows::Int64
-    maxcols::Int64
 end
 function Base.show(io::IO, m::TAB1OUT2)
     fmt = Printf.Format("%.$(m.digits)f")
@@ -40,7 +39,6 @@ function Base.show(io::IO, m::TAB1OUT2)
         formatters=[(v, _, j) -> isnan(v) ? "." : (j % 3 != 1 ? Printf.format(fmt, v) : string(v))],
         column_labels = m.colnames,
         maximum_number_of_rows = m.maxrows,
-        maximum_number_of_columns = m.maxcols,
         table_format=TextTableFormat(;
             @text__no_vertical_lines,
             horizontal_lines_at_column_labels=[1],
